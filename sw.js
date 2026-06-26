83b1b15795e71b25bd735776f1387c4e929ae243
Charly941 <carlosmunin@hotmail.com>
2026-06-26 12:55:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE = 'aventura-v3';
-const ASSETS = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// Unregister all service workers
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.matchAll({includeUncontrolled:true}))
+    .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
+  self.clients.claim();
 });
