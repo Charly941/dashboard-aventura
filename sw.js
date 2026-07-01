1eea939d6f7bbbe7b7ae39dbdfb20557e6a995ff
Charly941 <carlosmunin@hotmail.com>
2026-07-01 12:35:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-// Unregister all service workers
+// Versión 2 - auto-unregister
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.matchAll({includeUncontrolled:true}))
-    .then(clients => clients.forEach(c => c.navigate(c.url)))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({includeUncontrolled: true}))
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
-  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
